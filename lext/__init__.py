79e8f13b79fc75feb10786ac35cdfe47d4569e43
Iago-lito <iago-lito@etak>
2022-09-21 17:24:44 +0200

    Extract lext as a submodule.

--- lext/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import context
-from . import exceptions
-from .context import ContextLexer as Lexer
-from .exceptions import ParseError
-from .lexer import EOI, Lexer as BaseLexer
-from .parse_editor import ParseEditor
-from .parser import Parser
-from .reader import Reader, SplitAutomaton
